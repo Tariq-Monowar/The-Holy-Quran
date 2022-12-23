6c91cd86b4ce9a07e3a3b7771eb04676f8ff072b
Tariq-Monowar <101199109+Tariq-Monowar@users.noreply.github.com>
2022-12-23 22:17:44 +0600

    Update Artical.js

--- src/Components/Artical.js
+++ src/Components/Artical.js
@@ -2,42 +2,45 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Artical = (props) => {
-            const{
-            id,
-            name,
-            transliteration,
-            translation,
-            total_verses,
-            verses
-            } = props.AllAyat
+  const {
+    id,
+    name,
+    transliteration,
+    translation,
+    total_verses,
+    verses
+  } = props.AllAyat
+
   return (
-      <Link key={id} to={transliteration} 
-                state={{ 
-                      id,
-                      name,
-                      transliteration, 
-                      translation,
-                      total_verses, 
-                      verses
-                      }} 
-                 className = 'CardLink'>  
-                
-               <articl className="blogs">
-                 <div className="inner-blogs-left">
-                   <div className="ayat-number">
-                     <p>{id}</p>
-                   </div>
-                   <div className="sura-name">
-                    <p>{transliteration}</p>
-                    <p className='sura-translation-name'>{translation}</p>
-                   </div>
-                 </div>
-                   <div className="ayat-arabic-name">
-                   <p>{ name }</p>
-                   <p className='total-ayat-number'>ayah {total_verses}</p>
-                 </div> 
-               </articl>
-             </Link>    
+    <Link
+      key={id}
+      to={transliteration}
+      state={{
+        id,
+        name,
+        transliteration,
+        translation,
+        total_verses,
+        verses
+      }}
+      className="CardLink"
+    >
+      <articl className="blogs">
+        <div className="inner-blogs-left">
+          <div className="ayat-number">
+            <p>{id}</p>
+          </div>
+          <div className="sura-name">
+            <p>{transliteration}</p>
+            <p className="sura-translation-name">{translation}</p>
+          </div>
+        </div>
+        <div className="ayat-arabic-name">
+          <p>{name}</p>
+          <p className="total-ayat-number">ayah {total_verses}</p>
+        </div>
+      </articl>
+    </Link>
   )
 }
 
